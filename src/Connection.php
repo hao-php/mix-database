9c006a425560e5418f5d7665b7a405b9e956590a
quhao <quhaowk@163.com>
2026-03-11 22:09:53 +0800

    ...

--- src/Connection.php
+++ src/Connection.php
@@ -11,6 +11,18 @@ class Connection extends AbstractConnection
 
     protected $exceptional = false;
 
+    /**
+     * 重连重试次数
+     * @var int
+     */
+    protected $retryCount = 0;
+
+    /**
+     * 最大重连重试次数
+     * @var int
+     */
+    protected $maxRetries = 2;
+
     public function queryOne(int $fetchStyle = null)
     {
         return $this->call(__FUNCTION__, func_get_args());
@@ -51,11 +63,15 @@ class Connection extends AbstractConnection
         try {
             return call_user_func_array(parent::class . "::{$name}", $arguments);
         } catch (\Throwable $ex) {
-            if ($this->isDisconnectException($ex) && !$this->inTransaction()) {
+            if ($this->retryCount < $this->maxRetries && $this->isDisconnectException($ex) && !$this->inTransaction()) {
+                $this->retryCount++;
                 $this->reconnect();
+                // 重连后允许再次执行
                 $this->executed = false;
                 return $this->call($name, $arguments);
             } else {
+                // 不可在这里处理丢弃连接，会影响用户 try/catch 事务处理业务逻辑
+                // 会导致 commit rollback 时为 EmptyDriver
                 $this->exceptional = true;
                 throw $ex;
             }
